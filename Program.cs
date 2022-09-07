fc30585ff1fcbaa0cb6f4f734e75ccd193583479
luyencode <111958512+Anhsang2001CNTT@users.noreply.github.com>
2022-09-07 15:35:17 +0700

    Add files via upload
    hình chữ nhật

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
-public class Program
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+    namespace tinh
 {
-    static void Main(string[] args)
+    class Program
     {
-        var numbers = Console.ReadLine();
-        var numberList = numbers.Split(' ');
-        var number1 = Convert.ToInt32(numberList[0]);
-        var number2 = Convert.ToInt32(numberList[1]);
-        var number3 = Convert.ToInt32(numberList[2]);
-        Console.WriteLine(number1 + number2 + number3);
+        static void Main(String[] args)
+        {
+            double a, b, S, V;
+            Console.Write("nhap vao chieu dai a:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("nhop vao chieu rong b:");
+            b = Convert.ToDouble(Console.ReadLine());
+            S = a * b;
+            V = (a + b) * 2;
+            Console.Write($"Dien tich hinh chu nhat:{S} ");
+            Console.Write($"Chu vi hinh chu nhat:{V}");
+        }
     }
 }
+
+
