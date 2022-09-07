a8d3b550282a12cfc60f4b0d5e65a4181bf316c4
luyencode <111958512+Anhsang2001CNTT@users.noreply.github.com>
2022-09-07 15:32:07 +0700

    tổng 3 số nguyên

--- Program.cs
+++ Program.cs
@@ -1,24 +1,13 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
-using System.Collections.Generic;
-    namespace tinh
+public class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(String[] args)
-        {
-            double a, b, S, V;
-            Console.Write("nhap vao chieu dai a:");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.Write("nhop vao chieu rong b:");
-            b = Convert.ToDouble(Console.ReadLine());
-            S = a * b;
-            V = (a + b) * 2;
-            Console.Write($"Dien tich hinh chu nhat:{S} ");
-            Console.Write($"Chu vi hinh chu nhat:{V}");
-        }
+        var numbers = Console.ReadLine();
+        var numberList = numbers.Split(' ');
+        var number1 = Convert.ToInt32(numberList[0]);
+        var number2 = Convert.ToInt32(numberList[1]);
+        var number3 = Convert.ToInt32(numberList[2]);
+        Console.WriteLine(number1 + number2 + number3);
     }
 }
-
-
